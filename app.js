1519c34ac1775ba9cd843c02b8f2d9a97d67087e
Mickey374 <asantemichael37483@gmail.com>
2021-04-01 04:06:14 +0000

    Added an event listener and a random int generator for choosing colors

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const color = document.querySelector(".color");
 
 btn.addEventListener('click', function(){
     //get Random number from the array color
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
 
-    document.body.style.backgroundColor = color[randomNumber];
+    document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 })
+
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
